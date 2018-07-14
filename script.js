cbde30de248df8239aa2b3fabb815b486871ad17
root <root@localhost.localdomain>
2018-07-14 04:00:19 -0700

    initial

---
+++ script.js
@@ -0,0 +1,19 @@
+(function() { 
+     var li = document.createElement('script'); li.type = 'text/javascript'; li.async = true; 
+     li.src = window.location.protocol + '//platform.stumbleupon.com/1/widgets.js'; 
+     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(li, s); 
+})(); 
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+ 
+(function() {
+  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1&appId=231759450196872";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
